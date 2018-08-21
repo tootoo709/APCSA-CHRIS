b558be152d36f88a6b8e5420a85e51e44040a785
tootoo709 <christoamazing104@gmail.com>
2018-08-21 13:20:25 -0500

    APCSA Files 08/21/2018

---
+++ APCSA Activity Templates/Lesson1/Lesson_1_Activity_Three.java
@@ -0,0 +1,23 @@
+/*
+ * Lesson 1 Coding Activity Question 3
+ * 
+ * Write a program to output the following:
+
+    ********
+    * java *
+    ********
+
+ * For an extra challenge: complete this problem using only one line of code.
+*/
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_1_Activity_Three {
+    public static void main(String[] args) {
+     System.out.println("********");
+     System.out.println("* java *"); 
+     System.out.println("********");    
+
+    }
+}
---
+++ APCSA Activity Templates/Lesson1/Lesson_1_Activity_Two.java
@@ -0,0 +1,21 @@
+/*
+ * Lesson 1 Coding Activity Question 2
+ * 
+ * Write a program to print your name to the center of the output screen on three different lines.
+ * 
+ * For an extra challenge: complete this problem using only one line of code.
+*/
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_1_Activity_Two {
+    public static void main(String[] args) 
+    {
+        System.out.println(" Chris ");  
+        System.out.println(" Chris ");  
+        System.out.println(" Chris ");  
+        System.out.println(" \"Hello\" said the man.  ");  
+
+    }
+}
