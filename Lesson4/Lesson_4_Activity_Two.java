471c29c913c72dd46c0fcaffba44cfde0368c1d0
tootoo709 <christoamazing104@gmail.com>
2018-08-27 12:57:44 -0500

    j

--- Lesson4/Lesson_4_Activity_Two.java
+++ Lesson4/Lesson_4_Activity_Two.java
@@ -22,9 +22,9 @@ class Lesson_4_Activity_Two {
         Scanner scan = new Scanner(System.in); 
         System.out.println("Hi there. What is your name?");
         String name = scan.nextLine(); 
-        System.out.println("Hi" + name + "How old are you?"); 
-        Double age = scan.nextDouble();       
-        System.out.println(name + "You will be 100 in " + (100 - age) + "years"); 
+        System.out.println("Hi " + name + ". How old are you?"); 
+        int age = scan.nextInt();       
+        System.out.println(name + ", you will be 100 in " + (100 - age) + " years."); 
 
 
     }
