9516289030bf52429242a243ed4dfb749496d8e3
tootoo709 <christoamazing104@gmail.com>
2018-08-28 13:25:05 -0500

    f

---
+++ Lesson5/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Test {
+    
+    int a = 89;
+    double x = 78.2347;
+    System.out.println(a/2);
+    
+}
---
+++ Lesson6/Lesson_6_Activity_Two.java
@@ -0,0 +1,31 @@
+/*
+ * Lesson 6 Coding Activity Question 2
+ *
+ * Input a double value and print only the integer part.
+ *
+ * Sample run:
+ 
+Please input a decimal number:
+57.8934
+Answer: 57
+
+*/
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_6_Activity_Two {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+        System.out.println("Please input a decimal number: "); 
+        int number = (int)scan.nextDouble(); 
+        System.out.println(number); 
+            
+        /* Write your code here 
+         * Copy and paste your entire code to Code Runner to complete the activity, 
+         * from the first import statement to the last bracket. 
+        */
+
+
+    }
+}
